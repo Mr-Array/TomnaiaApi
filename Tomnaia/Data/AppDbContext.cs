54f64aa001e5e9706e1e9c7d1a92c85cf2da7301
Ahmed_Kamal <ahmed0a41468158@gmail.com>
2024-05-27 13:34:21 +0300

    edit cycle

--- Tomnaia/Data/AppDbContext.cs
+++ Tomnaia/Data/AppDbContext.cs
@@ -2,11 +2,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.DotNet.Scaffolding.Shared.Messaging;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Hosting;
-using System.Net.Mail;
-using Tomnaia.Data.Configuration;
 using Tomnaia.Entities;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Tomnaia.Data
 {
@@ -20,15 +16,37 @@ namespace Tomnaia.Data
         public DbSet<Ride> Rides { get; set; }
         public DbSet<Comment> Comments { get; set; }
       
-        public DbSet<Message> Messages { get; set; }
+        public DbSet<Entities.Message> Messages { get; set; }
 
        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<User>()
+                .HasMany(c => c.SentMessages)
+                .WithOne(u => u.Sender)
+                .HasForeignKey(c => c.SenderId)
+                .OnDelete(DeleteBehavior.Cascade);
 
-            
+            modelBuilder.Entity<User>()
+                .HasMany(c => c.ReceivedMessages)
+                .WithOne(u => u.Receiver)
+                .HasForeignKey(c => c.ReceiverId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Entities.Message>()
+                .HasOne(c => c.Sender)
+                .WithMany(u => u.SentMessages)
+                .HasForeignKey(c => c.SenderId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Entities.Message>()
+                .HasOne(c => c.Receiver)
+                .WithMany(u => u.ReceivedMessages)
+                .HasForeignKey(c => c.ReceiverId)
+                .OnDelete(DeleteBehavior.NoAction);
+
 
             SeedRoles(modelBuilder);
 
