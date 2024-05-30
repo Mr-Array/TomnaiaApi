80d4e715dc885022c187b5a668231f5044a7cd16
Ahmed_Kamal <ahmed0a41468158@gmail.com>
2024-05-30 22:47:00 +0300

    غ

--- Tomnaia/Services/Services/AuthService.cs
+++ Tomnaia/Services/Services/AuthService.cs
@@ -55,10 +55,10 @@ namespace Tomnaia.Services.Services
             if (result.Succeeded)
                 await _userManager.AddToRoleAsync(user, ConstsRoles.Driver);
 
-            //var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-            //var confirmationLink = $"{_contextAccessor.HttpContext.Request.Scheme}://{_contextAccessor.HttpContext.Request.Host}/api/authorization/confirm-email?email={Uri.EscapeDataString(user.Email)}&token={Uri.EscapeDataString(token)}";
-            //var message = new MailMessage(new string[] { user.Email }, "Confirmation email link", confirmationLink);
-            //_mailingService.SendMail(message);
+            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            var confirmationLink = $"{_contextAccessor.HttpContext.Request.Scheme}://{_contextAccessor.HttpContext.Request.Host}/api/authorization/confirm-email?email={Uri.EscapeDataString(user.Email)}&token={Uri.EscapeDataString(token)}";
+            var message = new MailMessage(new string[] { user.Email }, "Confirmation email link", confirmationLink);
+            _mailingService.SendMail(message);
             return result;
         }
 
